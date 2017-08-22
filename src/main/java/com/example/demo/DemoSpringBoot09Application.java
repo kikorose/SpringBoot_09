54ce2585645025b01aac5188432ad7208c1409db
kikorose <nakelia78@gmil.com>
2017-08-22 12:03:22 -0400

    first commit

---
+++ src/main/java/com/example/demo/DemoSpringBoot09Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBoot09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBoot09Application.class, args);
+	}
+}
